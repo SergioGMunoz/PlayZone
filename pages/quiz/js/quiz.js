6ef5afa1c0e2e42fb0126a4851fc67914e096f2c
SergioGonzlez <sergoyoyt@gmail.com>
2025-04-09 18:20:53 +0200

    Funcionalidad del contador generico timer.js y prueba en trio. Añadido boton flecha siguiente deshabilitada en pagina de trio

--- pages/quiz/js/quiz.js
+++
@@ -1,1 +0,0 @@
-// JavaScript funcionalidad de el juego quiz!
---
+++ pages/quiz/quiz.js
@@ -0,0 +1,13 @@
+// JavaScript funcionalidad de el juego quiz!
+
+//Estas funciones estan descritas en la hoja timer.js
+setTimeElement(document.getElementById('time'));
+setTime(10);
+startTimer();
+
+function endTime(){
+    //Función a rellenar con que pasa cuando se 
+    // acaba el tiempo
+    console.log("Se acabo el tiempo");
+}
+end = endTime; // Asociar la referencia
