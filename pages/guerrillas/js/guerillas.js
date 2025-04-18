31c440bbbaab8ed2a4fd40c2338809a717f24d0f
SergioGonzlez <sergoyoyt@gmail.com>
2025-04-18 21:12:23 +0200

    Comenzando estructura

---
+++ pages/guerrillas/js/guerillas.js
@@ -0,0 +1,26 @@
+// Valores de guerreros iniciales
+const templateWarriors = {
+    1 : {
+        img: 'img/Warrior1.svg',
+        cost: 2,
+        live: 4,
+        power: 2
+    },
+    2:  {
+        img: 'img/Warrior2.svg',
+        cost: 3,
+        live: 6,
+        power: 3
+    },
+    3:  {
+        img: 'img/Warrior3.svg',
+        cost: 4,
+        live: 5,
+        power: 5
+    }
+}
+
+// Guerreros situados en el campo de batalla
+const enemyWarriors = [null,null,null]
+const playerWarriors = [null,null,null]
+
