91091d63db13c7a43fbdb8987ef56b1cd27e9acd
SergioGonzlez <sergoyoyt@gmail.com>
2025-04-19 13:08:18 +0200

    Sistema de batalla

--- pages/guerrillas/guerillas.js
+++ pages/guerrillas/guerillas.js
@@ -4,20 +4,20 @@ const templateWarriors = {
     img: "img/Warrior1.svg",
     cost: 2,
     live: 4,
-    power: 2,
+    power: 2
   },
   2: {
     img: "img/Warrior2.svg",
     cost: 3,
     live: 6,
-    power: 3,
+    power: 3
   },
   3: {
     img: "img/Warrior3.svg",
     cost: 4,
     live: 5,
-    power: 5,
-  },
+    power: 5
+  }
 };
 
 // Guerreros situados en el campo de batalla
@@ -90,4 +90,5 @@ function initializeGame() {
 
 // Llamamos al cargar la página para que actualize todo
 document.addEventListener("DOMContentLoaded", initializeGame);
+
 
